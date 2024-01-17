326b93a297a4b03241c3201a218ee6afd5ac1038
el-kg <kirillgalkin8@gmail.com>
2024-01-17 20:52:43 +0700

    поправлен импорт Id в файле NotificationTask.java

--- src/main/java/pro/sky/telegrambot/notification_task/NotificationTask.java
+++ src/main/java/pro/sky/telegrambot/notification_task/NotificationTask.java
@@ -1,10 +1,9 @@
 package pro.sky.telegrambot.notification_task;
 
-import org.springframework.data.annotation.Id;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
